ae70e25bd98e776f9ec448723311e734fddaed3b
jx1279 <jx1279@nyu.edu>
2024-01-28 17:17:27 -0500

    Added Player Script Code to detect blocking

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    boolean acted;
+    boolean blocking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        acted = false;
+        blocking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space") && !acted) {
+            acted = true;
+            blocking = true;
+            StartCoroutine(waiting());
+            blocking = false;
+        }
+    }
+
+    IEnumerator waiting()
+    {
+        yield return new WaitForSeconds(0.5f);
+    }
+}
