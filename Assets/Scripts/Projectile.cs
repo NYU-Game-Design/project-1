5b556eab45c2c71b61a5c0d6979e47b9c515d41c
jx1279 <jx1279@nyu.edu>
2024-01-29 18:54:18 -0500

    Redid how the projectile code works

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,33 +4,24 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    private SceneChanger detectNext;
+    public Transform projectilePoint;
+    public GameObject projectilePrefab;
+
+    private GameObject bulletInst;
+
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(waiter());
-        if (FireRay()){
-            detectNext.LoadScene();
-        }
+        bulletInst = Instantiate(projectilePrefab, projectilePoint.position, projectilePoint.rotation);
+
         //Load game over screen
     }
 
     IEnumerator waiter()
     {
         yield return new WaitForSeconds(Random.Range(1f, 7f));
-    }
-
-    private bool FireRay()
-    {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right*range* transform.localScale.x*colliderDistance,
-            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
-            0, Vector2.left, 0, playerLayer);
-            // MAY NEED TO CHANGE THIS SO THAT IT CAN FIRE FROM A SPECIFIC LOCATION
-
-
-        if (hit.collider != null)
-            // Check to see if the target hit was the player
-        return hit.collider != null;
     }
 
     // Update is called once per frame
